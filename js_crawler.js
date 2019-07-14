41755d1f3e9880122166526d09dba076fb0af83f
Karavision <karavisiongithub@outlook.com>
2019-07-13 21:52:56 -0700

    Update js_crawler.js

--- js_crawler.js
+++ js_crawler.js
@@ -33,33 +33,33 @@ const magicStones = {
     yellow
 };
 
-var player = {            //All of the player data
+var player = { //All of the player data
     hp: 25,
     amr: 0,
     pwr: 0,
     wpn: '',
-    psn: 0,       //poison effect counter
-    luck: 0,      //luck effect counter
-    amrwdn: 0,    //turn counter for frost effect
-    red: 0,       //colors are inventory. They keep -
-    blue: 0,      //-players limited to one stone.
+    psn: 0, //poison effect counter
+    luck: 0, //luck effect counter
+    amrwdn: 0, //turn counter for frost effect
+    red: 0, //colors are inventory. They keep -
+    blue: 0, //-players limited to one stone.
     green: 0,
     yellow: 0,
-    cast: 0,     //used to determine is magic was used during magic subroutine
-    prevamr: 0   //used to reset AMR after frost effect
+    cast: 0, //used to determine is magic was used during magic subroutine
+    prevamr: 0 //used to reset AMR after frost effect
 };
-var enemy = {            //All of the player data
+var enemy = { //All of the player data
     hp: 25,
     amr: 5,
     pwr: 32,
     wpn: 'Bone',
-    psn: 0,       //poison effect counter
-    luck: 6,      //luck effect counter
-    amrwdn: 0,    //turn counter for frost effect
-    red: 0,       //colors are inventory. They keep -
-    blue: 0,      //-players limited to one stone.
+    psn: 0, //poison effect counter
+    luck: 6, //luck effect counter
+    amrwdn: 0, //turn counter for frost effect
+    red: 0, //colors are inventory. They keep -
+    blue: 0, //-players limited to one stone.
     green: 0,
     yellow: 0,
-    cast: 0,     //used to determine is magic was used during magic subroutine
-    prevamr: 5   //used to reset AMR after frost effect
+    cast: 0, //used to determine is magic was used during magic subroutine
+    prevamr: 5 //used to reset AMR after frost effect
 };
