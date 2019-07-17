06a8f65262ca37e15904de0561658b465dc2ca35
Karavision <karavisiongithub@outlook.com>
2019-07-16 23:02:49 -0700

    Added bChoice and battle functions. They are unfinished

--- js_crawler.js
+++ js_crawler.js
@@ -46,7 +46,8 @@ var player = { //All of the player data
     green: 0,
     yellow: 0,
     cast: 0, //used to determine is magic was used during magic subroutine
-    prevamr: 0 //used to reset AMR after frost effect
+    prevamr: 0, //used to reset AMR after frost effect
+    won: 0
 };
 var enemy = { //All of the enemy data
     hp: 25,
@@ -68,27 +69,93 @@ function diceRoll(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
-  }
+}
 
 // My next step is to recreate the main battle functions
+function battle(player, enemy) {
+    while (won == 0) {
+        document.getElementById("bMenu").style.visibility = "visible";
+        document.getElementById("bMenu").style.opacity = "1";
+        document.getElementById('attack').onclick = bChoice(this.id, player, enemy);
+        document.getElementById('defend').onclick = bChoice(this.id, player, enemy);
+        document.getElementById('magic').onclick = bChoice(this.id, player, enemy);
+    }
+    if (won == 1) {
+        return;
+    }}
+
+function bChoice(clicked_id, player, enemy) {
+    var choiceBttn = clicked_id;
+    document.getElementById("bMenu").style.visibility = "hidden";
+    switch (choiceBttn) {
+        case "attack":
+            choiceBttn = "disable";
+            pTurn(player, enemy);
+            eTurn(player, enemy);
+            break;
+    }
+    return;
+}
+
 function pTurn(player, enemy) {
+    if (player.won == 1) {
+        return;
+    }
     var dmg;
     var tempRoll = diceRoll(1, 20);
     if (tempRoll == 20) {
-        dmg = (player.pwr * 1.5);}
-    else if (tempRoll <= 2) {
-        critMiss(player, enemy);}
-    else {
+        dmg = (player.pwr * 1.5);
+    } else if (tempRoll <= 2) {
+        critMiss(player, enemy);
+    } else {
         tempRoll = diceRoll(1, 20);
         dmg = (Math.round(player.pwr / tempRoll) * (enemy.amr * .1));
     }
     enemy.hp = (enemy.hp - dmg);
-
-    
+    if (enemy.hp < 1) {
+        win();
     }
+    hitEmote();
+    return;
+}
+
+function eTurn(player, enemy) {
+    if (player.won == 1) {
+        return;
+    }
+    var dmg;
+    var tempRoll = diceRoll(1, 20);
+    if (tempRoll == 20) {
+        dmg = (enemy.pwr * 1.5);
+    } else if (tempRoll <= 2) {
+        critMiss(player, enemy);
+    } else {
+        tempRoll = diceRoll(1, 20);
+        dmg = (Math.round(enemy.pwr / tempRoll) * (player.amr * .1));
+    }
+    player.hp = (player.hp - dmg);
+    if (player.hp < 1) {
+        lose();
+    }
+    hitEmote();
+    return;
 }
 
 function critMiss(player, enemy) {
+
+
+}
+
+function win() {
+    player.won = 1;
+}
+
+function hitEmote() {
+
+
+}
+
+function lose() {
 
 
 }
